036da7ebae2bac599ad76acf552083a4a3d9afa3
francodimasi <francodimasi@hotmail.com>
2016-12-28 12:31:07 -0300

    fix array bar chart

--- src/app/data/data.component.ts
+++ src/app/data/data.component.ts
@@ -25,14 +25,9 @@ export class DataComponent implements OnInit {
     this.dataService.getTweets()
       .subscribe((tweets: any) => {
 
-        // let followersArea: ChartConfig = {
-        //   settings: {
-        //     fill: 'rgba(1, 67, 163, 1)',
-        //     interpolation: 'monotone'
-        //   }, dataset: tweets.docs.map(data => {
-        //     return { x: new Date(data.created_at), y: data.followers_count };
-        //   })
-        // };
+        // Bubble Chart service
+        //
+        //
 
         let bubbleChartArea: BubbleChartConfig = {
           dataset: tweets.docs.map(data => {
@@ -43,47 +38,28 @@ export class DataComponent implements OnInit {
         this.BubbleChartConfig = new Array<BubbleChartConfig>();
         this.BubbleChartConfig.push(bubbleChartArea);
 
-        // let barChartArea: BarChartConfig = {
-        //   columns: tweets.docs.map(data => {
-        //     return { intents: data.intents[0].intent };
-        //   })
-        // };
+        // Bar Chart Service
+        //
+        //
+        
+        let barChartAreaTemp = tweets.docs.reduce(function(intents, data) {
+          if (data.intents[0].intent in intents) {
+            intents[data.intents[0].intent]++;
+          }
+          else {
+            intents[data.intents[0].intent] = 1;
+          }
+          return intents;
+        }, {});
 
-        let barChartArea: BarChartConfig = {
-          columns: tweets.docs.reduce(function(intents, data) {
-            if (data.intents[0].intent in intents) {
-              intents[data.intents[0].intent]++;
-            }
-            else {
-              intents[data.intents[0].intent] = 1;
-            }
-            return intents;
-          }, {})
-        };
+        let barChartArea: BarChartConfig = { columns:[] };
 
-        // console.log(barChartArea);
-
-        // var countedNames = names.reduce(function(allNames, name) {
-        //   if (name in allNames) {
-        //     allNames[name]++;
-        //   }
-        //   else {
-        //     allNames[name] = 1;
-        //   }
-        //   return allNames;
-        // }, {});
-
+        Object.keys(barChartAreaTemp).forEach(function(intent) {
+          barChartArea.columns.push({name: intent, count: barChartAreaTemp[intent]});
+        });
 
         this.BarChartConfig = barChartArea;
 
-        // console.log(this.BarChartConfig);
-
-        // console.log(this.BarChartConfig);
-
-        // console.log(barChartArea);
-
-        // this.ChartConfig = new Array<ChartConfig>();
-        // this.ChartConfig.push(bubbleChartArea);
       });
   }
 
